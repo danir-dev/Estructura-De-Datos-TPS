fc214492a9bad9a9e725f95e167ddbc3856419c1
Daniel Ramos <danielramos550455@gmail.com>
2024-06-17 16:56:25 -0300

    TPS EDD

---
+++ src/TP0/EP1.java
@@ -0,0 +1,21 @@
+/* 
+    Dadas las longitudes de cuatro segmentos correspondientes a una figura geométrica, determinar si la
+    misma se trata de un cuadrado, de un rectángulo o de otro polígono. Si la figura es un cuadrado, calcular su
+    perímetro. Si es un rectángulo, calcular su superficie. La entrada de datos debe continuar en tanto los
+    segmentos ingresados sean valores positivos. Determinar la mayor superficie y el menor perímetro
+    calculados. Además, informar cuantas figuras de las dadas no corresponden ni a un cuadrado ni a un
+    rectángulo.
+    Indicaciones:
+    Este ejercicio necesita del objeto scanner para ingresar datos por la consola o teclado, se espera que el
+    código controle los problemas que normalmente ocurren al operar con la consola o teclado.
+    Se espera una correcta modularización entre el código que realiza el ingreso y validación de los datos
+    respecto del código que hace lo que se solicita en el ejercicio.
+ */
+
+package TP0;
+
+public class EP1 {
+
+    
+
+}
